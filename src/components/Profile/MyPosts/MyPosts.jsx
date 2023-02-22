0b3e311c707f060e60df15a51739727c25281932
Zhambyl Aldongar <aldongarovzh@aldzh.local>
2023-02-22 21:29:48 +0600

    22.02.23

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,11 +11,8 @@ const MyPosts = () => {
                     <button> Add post </button>
                 </div>
                 <div className={s.posts}>
-                    <Post />
-                    <Post />
-                    <Post />
-                    <Post />
-                    <Post />
+                    <Post message='Hi, how are you?' like='likes=15' />
+                    <Post message="It's my first post" like='likes=20' />
                 </div>
             </div>
     )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
-                    <div className={s.item}>
-                        <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=1380&t=st=1676316110~exp=1676316710~hmac=24cc4d64d0c5e9157c3751223d076cb888ef53c355d748eee718e4e504e50c39" alt=""/>
-                        post 1
-                        <div>
-                            <span> Like </span>
-                        </div>
-                    </div>
+            <div className={s.item}>
+                <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=1380&t=st=1676316110~exp=1676316710~hmac=24cc4d64d0c5e9157c3751223d076cb888ef53c355d748eee718e4e504e50c39" alt=""/>
+                {props.message}
+                <br/>
+                {props.like}
+                <div>
+                    <span> Like </span>
+                </div>
+            </div>
     )
 }
 
