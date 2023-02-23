53b1e5d66d17c7c28d02aab7e343124454df5c5c
Zhambyl Aldongar <aldongarovzh@aldzh.local>
2023-02-23 23:01:18 +0600

    les19 before input a command npm i react-router-dom

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import s from './Navbar.module.css';
 
 
-
 const Navbar = () => {
     return (
         <nav className='s.nav'>
-                <div className={s.item}>
-                    <a>Profile</a>
-                </div>
-                <div className={s.item}>
-                    <a href="src/components/Navbar/Navbar#">Messages</a>
-                </div>
-                <div className={s.item}>
-                    <a href="src/components/Navbar/Navbar#">News</a>
-                </div>
-                <div className={s.item}>
-                    <a href="src/components/Navbar/Navbar#">Music</a>
-                </div>
-                <div className={s.item}>
-                    <a href="src/components/Navbar/Navbar#">Settings</a>
-                </div>
+            <div className={s.item}>
+                <a href="/profile">Profile</a>
+            </div>
+            <div className={s.item}>
+                <a href="/dialogs">Messages</a>
+            </div>
+            <div className={s.item}>
+                <a href="">News</a>
+            </div>
+            <div className={s.item}>
+                <a href="">Music</a>
+            </div>
+            <div className={s.item}>
+                <a href="">Settings</a>
+            </div>
         </nav>
     )
 }
