e1e0f250930b9402f751df1eee83bfa30417d954
Zhambyl Aldongar <aldongarovzh@aldzh.bbrouter>
2023-02-25 20:35:02 +0600

    before Navlink to l20

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
 
 function App() {
     return (
@@ -16,6 +18,8 @@ function App() {
                     <Routes>
                         <Route path="/dialogs" element={<Dialogs/>}/>
                         <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/news" element={<News/>}/>
+                        <Route path="/music" element={<Music/>}/>
                     </Routes>
                 </div>
             </div>
