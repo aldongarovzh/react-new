b8d91f5ffdcfe640d4a40da4c8f2b0083a8e071e
Zhambyl Aldongar <aldongarovzh@aldzh.local>
2023-02-28 23:54:02 +0600

    Dialogs and Messages | Name and Message less 22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,28 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Amir
+                </div>
+                <div className={s.dialog}>
+                    Qanat
+                </div>
+                <div className={s.dialog}>
+                    Rustem
+                </div>
+                <div className={s.dialog}>
+                    Roma
+                </div>
+            </div>
+
+
+            <div className={s.messages}>
+                <div className={s.message}> Hello </div>
+                <div className={s.message}> How is your IT </div>
+                <div className={s.message}> Yo </div>
+            </div>
         </div>
     );
 }
