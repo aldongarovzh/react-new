1a015fc670ae480c531b4a470d331bceddbd8d93
Zhambyl Aldongar <aldongarovzh@aldzh.local>
2023-02-28 23:23:10 +0600

    color: gold doesnt work| im going to next less

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 
 const Navbar = () => {
     return (
         <nav className='s.nav'>
             <div className={s.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile" className={(navData) => navData.isActive ? 's.active' : 's.item'}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" className={(navData) => navData.isActive ? 's.active' : 's.item'}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music">Music</NavLink>
             </div>
             <div className={s.item}>
                 <a href="">Settings</a>
